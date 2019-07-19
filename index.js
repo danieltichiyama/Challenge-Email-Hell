beb19c27fd30bdedc7003accf55da8ca7dcf6e30
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-07-18 18:22:09 -1000

    finished, ready for review

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const emailLog = require('./email_log.json');
+const emailLog = require("./email_log.json");
 
 function countUniqueEmails(emailFile) {
   // do work here
+  const theList = emailFile.emails; //this is an array
+
+  return theList.reduce(function(p, c) {
+    let email = c.email;
+    if (!p[email]) {
+      p[email] = 1;
+    } else {
+      p[email]++;
+    }
+    return p;
+  }, {});
 }
 
 console.log(countUniqueEmails(emailLog));
